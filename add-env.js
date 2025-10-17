02475d66f53c5c79b9b6c6088cb264a929b440f4
Saif <warriorsaifdurer@gmail.com>
2025-10-17 21:16:03 +0530

    Add Vercel project link and env setup script

---
+++ add-env.js
@@ -0,0 +1,8 @@
+// Simple script to show environment variables that need to be added to Vercel
+console.log('Add these environment variables to Vercel:')
+console.log('')
+console.log('WHATSAPP_INSTANCE_ID = 7107348623')
+console.log('WHATSAPP_ACCESS_TOKEN = 1b4aeb0887eb46b7bbf62d91dec529d52e40d041db624046b2')
+console.log('WHATSAPP_ENABLED = true')
+console.log('')
+console.log('Go to: https://vercel.com/dashboard → loyallinkk → Settings → Environment Variables')
